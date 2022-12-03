3e70345e92dcb9c2af82af81f7446e03f1a90f5a
namanpatel <namanpatel1997@yahoo.com>
2022-12-03 15:42:20 -0600

    added comment

--- src/main/java/com/namanpat/AddTwoIntegers.java
+++ src/main/java/com/namanpat/AddTwoIntegers.java
@@ -3,6 +3,8 @@ public class AddTwoIntegers {
 
     public static void main(String[] args) {
 
+	//Add a comment here
+	//This is how it wors
         AddTwoIntegers n1 = new AddTwoIntegers();
         int result = n1.addTwoIntegers(1,2);
         System.out.println("The sum of numbers is: " + result);
